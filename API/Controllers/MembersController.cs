cb5df0e869686557b8294e8c53d00cdd37f599df
Paolo Paci <paolopci@yahoo.it>
2025-12-18 17:17:03 +0100

    Refactor MembersController to use async methods for user retrieval

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using API.Data;
 using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace API.Controllers;
@@ -10,16 +11,16 @@ namespace API.Controllers;
 public class MembersController(AppDataContext context) : ControllerBase
 {
     [HttpGet]
-    public ActionResult<IReadOnlyList<AppUser>> GetMembers()
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
     {
-        var members = context.Users.ToList();
+        var members = await context.Users.ToListAsync();
         return Ok(members);
     }
 
     [HttpGet("{id}")]
-    public ActionResult<AppUser> GetMember(string id)
+    public async Task<ActionResult<AppUser>> GetMember(string id)
     {
-        var member = context.Users.Find(id);
+        var member = await context.Users.FindAsync(id);
         if (member == null)
         {
             return NotFound();
