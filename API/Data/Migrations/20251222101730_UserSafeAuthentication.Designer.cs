2d677c20f931dcc215b2155b9b149d007406bf53
Paolo Paci <paolopci@yahoo.it>
2025-12-22 11:40:30 +0100

    feat: implement user authentication fields with PasswordHash and PasswordSalt in AppUser entity and migrations

--- API/Data/Migrations/20251222101730_UserSafeAuthentication.Designer.cs
+++ API/Data/Migrations/20251222101730_UserSafeAuthentication.Designer.cs
@@ -3,6 +3,7 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(AppDataContext))]
-    partial class AppDataContextModelSnapshot : ModelSnapshot
+    [Migration("20251222101730_UserSafeAuthentication")]
+    partial class UserSafeAuthentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
